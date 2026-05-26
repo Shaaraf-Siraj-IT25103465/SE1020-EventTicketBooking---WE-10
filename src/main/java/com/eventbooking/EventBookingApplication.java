43e14c9fbe040634b408232fe0fc3bd91f84b872
Shaaraf-Siraj-IT25103465 <shaarafsiraj@gmail.com>
2026-05-26 08:14:40 +0530

    fix: Add @ServletComponentScan & Update application.properties

--- src/main/java/com/eventbooking/EventBookingApplication.java
+++ src/main/java/com/eventbooking/EventBookingApplication.java
@@ -2,8 +2,10 @@ package com.eventbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class EventBookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventBookingApplication.class, args);
